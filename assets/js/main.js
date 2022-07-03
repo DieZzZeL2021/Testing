674fef26e2b71f2d3699665db42b02ba678e072e
DieZzZeL2021 <syzzzam@gmail.com>
2022-07-03 18:15:57 +0300

    Тестирование кода

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/** @format */
+
+function ageClassification(n) {
+  var str = '';
+
+  str =
+    n < 0
+      ? null
+      : n <= 24
+      ? 'детский возраст'
+      : n <= 44
+      ? 'молодой возраст'
+      : n <= 65
+      ? 'средний возраст'
+      : n <= 75
+      ? 'пожилой возраст'
+      : n <= 90
+      ? 'старческий возраст'
+      : n <= 122
+      ? 'долгожители'
+      : null;
+  return str;
+}
+
+console.log(ageClassification(2));
